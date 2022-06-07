e3b2c7dc321b5f95b85897a2085345f3644ee264
Ali Chraghi <63465728+alichraghi@users.noreply.github.com>
2022-06-07 18:38:42 +0430

    add harfbuzz upstream

--- update-upstream.sh
+++ update-upstream.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-rm -rf freetype/
+rm -rf freetype/ harfbuzz/
 git clone --depth 1 --branch VER-2-12-1 https://github.com/freetype/freetype
-cd freetype/
+git clone --depth 1 --branch 4.3.0 https://github.com/harfbuzz/harfbuzz
 
-# Remove non-C files
+# --- FreeType ---
+pushd freetype/
 rm -rf .git/ .clang-format .gitlab-ci.yml .mailmap .gitignore .gitmodules
 rm autogen.sh CMakeLists.txt configure Makefile meson.build meson_options.txt \
     modules.cfg README README.git vms_make.com src/tools/*.c
@@ -23,3 +24,19 @@ mv unix/ftsystem.c ../builds/unix/
 mv windows/ftsystem.c windows/ftdebug.c ../builds/windows
 popd
 rm -r tmp
+popd
+
+# --- HarfBuzz ---
+pushd harfbuzz/
+rm -rf .git/ .ci/ .circleci/ .github/ .clang-format .codecov.yml .editorconfig
+rm AUTHORS NEWS README THANKS configure.ac Makefile.am meson.build \
+    replace-enum-strings.cmake harfbuzz.doap BUILD.md CONFIG.md README.md \
+    README.mingw.md README.python.md RELEASING.md TESTING.md git.mk autogen.sh \
+    mingw-configure.sh CMakeLists.txt meson_options.txt
+rm -r docs/ m4/ perf/ subprojects/ test/ util/ src/ms-use src/OT
+find ./src -type f \
+    ! -iname '*.cc' -a \
+    ! -iname '*.hh' -a \
+    ! -iname '*.h' \
+    -exec rm -f {} +
+popd
